18700a37ae875cf962bd861238e9a8dd30419252
Goi <126573272+Vinicius-Goi@users.noreply.github.com>
2023-07-18 11:21:33 -0300

    correção ex91

--- Exercicios/Ex99.py
+++ Exercicios/Ex99.py
@@ -1,13 +1,17 @@
 def maior(*num):
+    cont = mai = 0
     print('-='*30)
     print('Analisando os valores passados...')
     for c in num:
         print(c, end=' ')
-        mai = c
-        if c > mai:
+        if cont == 0:
             mai = c
+        else:
+            if c > mai:
+                mai = c
+        cont += 1
     print(f'Foram informados {len(num)} valores ao todo.')
-    print(f'O maior valor informado foi {c}.')
+    print(f'O maior valor informado foi {mai}.')
 
 maior(2,9,4,5,7,1)
 maior(4,7,0)
